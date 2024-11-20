82a9b4c2d5dcd0aca5d87fb4c3e06a039e0335b8
Nass Saki <nasssaki@Nasss-MBP.home>
2024-11-20 13:02:23 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+let board;
+let turn;
+let winner;
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init(); 
+//Initialize all state variables then call render()
+function init () {
+turn= 1; 
+winner=null;
+
+}
